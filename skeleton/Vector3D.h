8b2ff0c999af6e33d242bd939cd2dc3f040e9476
Macarr0n <adri.arbas@gmail.com>
2026-09-18 10:43:56 +0200

    Actividad 1 y 2, P0

---
+++ skeleton/Vector3D.h
@@ -0,0 +1,63 @@
+#pragma once
+#include <PxPhysics.h>
+
+using namespace std;
+
+class Vector3D
+{
+private:
+	float x, y, z;
+
+public:
+	Vector3D() : x(0), y(0), z(0) {};
+	Vector3D(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {};
+	Vector3D(physx::PxVec3 vec) : x(vec.x), y(vec.y), z(vec.z) {};
+
+	float magnitude() const {
+		return sqrt(x * x + y * y + z * z);
+	}
+
+	Vector3D normalize() const {
+		float modulo = magnitude();
+		return Vector3D(x / modulo, y / modulo, z / modulo);
+	}
+
+	float dot(const Vector3D& v) const {
+		return (x * v.x + y * v.y + z * v.z);
+	}
+
+	Vector3D cross(const Vector3D& v) const {
+		return Vector3D((y * v.z - z * v.y), (z * v.x - x * v.z), (x * v.y - y * v.x));
+	}
+
+	Vector3D& operator= (const Vector3D& other) {
+		x = other.x;
+		y = other.y;
+		z = other.z;
+		return Vector3D(x, y, z);
+	}
+
+	Vector3D& operator+ (const Vector3D& other) {
+		return Vector3D(x + other.x, y + other.y, z + other.z);
+	}
+
+	Vector3D& operator- (const Vector3D& other) {
+		return Vector3D(x - other.x, y - other.y, z - other.z);
+	}
+
+	Vector3D& operator* (float op) {
+		return Vector3D(x * op, y * op, z * op);
+	}
+
+	Vector3D& operator+= (const Vector3D& other) {
+		x = x + other.x;
+		y = y + other.y;
+		z = z + other.z;
+		return Vector3D(x, y, z);
+	}
+
+	Vector3D operator physx::PxVec3() const {
+
+	}
+};
+
